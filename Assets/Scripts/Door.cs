7a3a230e3e724217face97fa23b10e0b56ac201b
joon0017 <21900699@handong.ac.kr>
2025-07-30 00:34:49 +0900

    Revert "Merge pull request #13 from joon0017/yy"
    This reverts commit f05e07d1b3141cee9184afa920527e3bc7a5225d, reversing
    changes made to 4d1587ed1284630d7eba7c7da8a8517309ba8882.

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -1,29 +1,8 @@
 using UnityEngine;
-using System.Collections;
 
 public class Door : MonoBehaviour
 {
-    [SerializeField] private GameObject burnTree;
-    [SerializeField] private float Duration = 3f;
-    /*
-        child 컴포넌트를 active로 바꾸고 3초후 openDoor
-    */
-    public void openDoor()
-    {
-        StartCoroutine(burn(burnTree));
-    }
-    private IEnumerator burn(GameObject obj)
-    {
-        Debug.Log("1");
-        if (obj == null)
-            yield break;
-
-        Debug.Log("2");
-        obj.SetActive(true);
-        // Stay
-        yield return new WaitForSeconds(Duration);
-        Debug.Log("3");
-        
+    public void openDoor(){
         Destroy(gameObject);
     }
 }
