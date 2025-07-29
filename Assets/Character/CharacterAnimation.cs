cd310f2445f320c7efd4742ad05344352b8b106f
joon0017 <21900699@handong.ac.kr>
2025-07-30 00:18:51 +0900

    character animation (both sides)

--- Assets/Character/CharacterAnimation.cs
+++ Assets/Character/CharacterAnimation.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class CharacterAnimation : MonoBehaviour
 {
     [SerializeField] private Animator anim;
+    private string lastState;
     void Start(){
         anim = gameObject.GetComponent<Animator>();
     }
@@ -11,12 +12,24 @@ public class CharacterAnimation : MonoBehaviour
     {
         if(InputManager.Instance.move == new Vector2(0,0)){
             anim.SetBool("isWalk",false);
+            anim.SetBool("isWalkLeft",false);
+            anim.SetBool("isWalkRight",false);
             
         }
         else{
-            if(InputManager.Instance.move.x < 0) gameObject.transform.eulerAngles = new Vector3(0, 180, 0);
-            else if(InputManager.Instance.move.x > 0) gameObject.transform.eulerAngles = new Vector3(0,0,0);
             anim.SetBool("isWalk",true);
+            if(InputManager.Instance.move.x < 0) {
+                anim.SetBool("isWalkLeft",true);
+                anim.SetBool("isWalkRight",false);
+                lastState = "isWalkLeft";
+            }
+            else if(InputManager.Instance.move.x > 0) {
+                anim.SetBool("isWalkRight",true);
+                anim.SetBool("isWalkLeft",false);
+                lastState = "isWalkRight";
+            }
+
+            anim.SetBool(lastState,true);
         }
     }
 }
